e0b0edd50e1886c43b3499678375106397fa9e5b
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-03-19 17:24:18 -0400

    feat: move eza into zsh configuration
    although yes, eza is separate and stuff, its part of the configuration i
    want to provide with a zsh.

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -26,6 +26,20 @@
 
   # User
   home-manager.users.frontear = { config, ... }: {
+    programs.eza = {
+      enable = true;
+
+      extraOptions = [
+        "--group"
+        "--group-directories-first"
+        "--header"
+        "--octal-permissions"
+      ];
+
+      git = true;
+      icons = true;
+    };
+
     programs.zsh = {
       enable = true;
 
