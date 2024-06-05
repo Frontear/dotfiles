153d6b90086d56546f5d257602f8d6d6d9c90fb8
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-06-05 03:05:05 -0400

    fix: desktop-wsl

--- hosts/desktop-wsl/default.nix
+++ hosts/desktop-wsl/default.nix
@@ -6,12 +6,15 @@
     inputs.home-manager.nixosModules.home-manager
     inputs.nixos-wsl.nixosModules.default
 
-    outputs.programs.direnv
-    outputs.programs.git
     outputs.programs.gpg
     outputs.programs.neovim
     outputs.programs.zsh
   ];
+
+  frontear.programs = {
+    direnv.enable = true;
+    git.enable = true;
+  };
 
   wsl = {
     enable = true;
