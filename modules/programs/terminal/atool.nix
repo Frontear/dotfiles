2500f7fb285ffca23320b6b29dfcaf782931eab3
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-06-24 12:27:37 -0400

    feat: atool module it looks a bit bad imo. I should be doing wrapProgram shit, but here we are.
    Maybe wrapper-manager is a good move now :D

---
+++ modules/programs/terminal/atool.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+let
+  inherit (lib) mkIf;
+
+  cfg = config.frontear.programs.terminal;
+in {
+  config = mkIf cfg.enable {
+    users.extraUsers.frontear.packages = with pkgs; [
+      (writeShellApplication {
+        name = "atool";
+
+        runtimeInputs = with pkgs; [
+          file
+          gnutar
+          gzip
+          bzip2
+          pbzip2
+          lzip
+          plzip
+          lzop
+          xz # lzma
+          zip unzip
+          rar unrar
+          lha
+          # unace
+          arj
+          rpm
+          cpio
+          # arc nomarch
+          p7zip
+          #unalz
+        ];
+
+        text = ''
+          ${lib.getExe pkgs.atool} "$@"
+        '';
+      })
+    ];
+  };
+}
