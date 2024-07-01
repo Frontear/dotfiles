2de7d39342fe67fd3ff12ecd854ee9b80d054b01
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-07-01 19:42:27 -0400

    feat: move all to flake-parts nix/ directory

--- flake.nix
+++ flake.nix
@@ -12,13 +12,13 @@
       url = "github:nix-community/impermanence";
     };
 
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware";
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware";
     };
 
     nixos-wsl = {
@@ -26,39 +26,17 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-vscode-extensions = {
-      url = "github:nix-community/nix-vscode-extensions";
+    nixvim = {
+      url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }: flake-parts.lib.mkFlake { inherit inputs; specialArgs = { inherit (inputs) nixpkgs home-manager impermanence nix-vscode-extensions nixos-hardware nixos-wsl nixvim; }; } {
     imports = [
       ./nix
     ];
+
     systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-    flake = {
-      nixosConfigurations = {
-        "LAPTOP-3DT4F02" = nixpkgs.lib.nixosSystem {
-          modules = [
-            self.nixosModules.default
-
-            (import ./hosts/common { inherit (inputs) home-manager; })
-            (import ./hosts/laptop { inherit (inputs) nixos-hardware; })
-          ];
-        };
-
-        "nixos" = nixpkgs.lib.nixosSystem {
-          modules = [
-            self.nixosModules.default
-
-            (import ./hosts/common { inherit (inputs) home-manager; })
-            (import ./hosts/desktop-wsl { inherit (inputs) nixos-wsl; })
-          ];
-        };
-      };
-
-      nixosModules.default = import ./modules { inherit (inputs) impermanence nixvim nix-vscode-extensions; };
-    };
   };
 }
--- nix/default.nix
+++ nix/default.nix
@@ -1,5 +1,40 @@
-{ ... }: {
+{
+  self,
+  nixpkgs,
+  home-manager,
+  impermanence,
+  nix-vscode-extensions,
+  nixos-hardware,
+  nixos-wsl,
+  nixvim,
+  ...
+}:
+{
   perSystem = { pkgs, ... }: {
     devShells.default = pkgs.callPackage ./shell.nix { };
+  };
+
+  flake = {
+    nixosConfigurations = {
+      "LAPTOP-3DT4F02" = nixpkgs.lib.nixosSystem {
+        modules = [
+          self.nixosModules.default
+
+          (import ../hosts/common { inherit home-manager; })
+          (import ../hosts/laptop { inherit nixos-hardware; })
+        ];
+      };
+
+      "nixos" = nixpkgs.lib.nixosSystem {
+        modules = [
+          self.nixosModules.default
+
+          (import ../hosts/common { inherit home-manager; })
+          (import ../hosts/desktop-wsl { inherit nixos-wsl; })
+        ];
+      };
+    };
+
+    nixosModules.default = import ../modules { inherit impermanence nix-vscode-extensions nixvim; };
   };
 }
