d98ad5b321888a58bb7f0e78568f1b61cf7fc2f2
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-08-10 04:04:56 -0400

    modules: pipewire halving battery my word..

--- modules/system/audio/pipewire/default.nix
+++ modules/system/audio/pipewire/default.nix
@@ -4,26 +4,38 @@
   ...
 }:
 let
-  inherit (lib) mkEnableOption mkForce mkIf;
+  inherit (lib) mkEnableOption mkForce mkIf mkMerge;
 in {
   options.my.system.audio.pipewire.enable = mkEnableOption "pipewire";
 
-  config = mkIf config.my.system.audio.pipewire.enable {
-    # Explicitly disable pulseaudio and alsa
-    hardware.pulseaudio.enable = mkForce false;
-    hardware.alsa.enablePersistence = false; # https://github.com/NixOS/nixpkgs/issues/330606
+  config = mkIf config.my.system.audio.pipewire.enable (mkMerge [
+    {
+      # Explicitly disable pulseaudio and alsa
+      hardware.pulseaudio.enable = mkForce false;
+      hardware.alsa.enablePersistence = false; # https://github.com/NixOS/nixpkgs/issues/330606
 
-    # PipeWire benefits from having realtime priority
-    security.rtkit.enable = true;
+      # PipeWire benefits from having realtime priority
+      security.rtkit.enable = true;
 
-    # Enable all pipewire related backends, for maximum compatibility
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-      wireplumber.enable = true;
-    };
-  };
+      # Enable all pipewire related backends, for maximum compatibility
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+        wireplumber.enable = true;
+      };
+    }
+    {
+      # https://www.reddit.com/r/linux/comments/1em8biv/psa_pipewire_has_been_halving_your_battery_life/
+      services.pipewire.wireplumber.extraConfig."10-disable-camera" = {
+        "wireplumber.profiles" = {
+          "main" = {
+            "monitor.libcamera" = "disabled";
+          };
+        };
+      };
+    }
+  ]);
 }
