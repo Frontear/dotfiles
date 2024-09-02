c64e2e183f92d71feaf6b684eaf42dc1ea8675bc
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-09-02 14:53:11 -0400

    major: simplify flake input caching

--- nix/cachix/default.nix
+++ nix/cachix/default.nix
@@ -10,23 +10,7 @@
     let
       # We want an instance of pkgs that has host-specific config and overlays applied
       pkgs = self.nixosConfigurations.${host}.pkgs;
-      inputFarm = (inputs:
-      let
-        isTrivial = input: !isType "flake" input || input.inputs == {};
-        mkDrv = name: path: pkgs.linkFarm "input-${name}-${toString path.lastModified}" (singleton { inherit name path; });
-      in pipe (removeAttrs inputs (singleton "self")) [
-        (mapAttrsToList (name: path:
-          if isTrivial path then
-            (mkDrv name path)
-          else
-            ((inputFarm path.inputs) ++ (singleton (mkDrv name path)))
-        ))
-        flatten
-      ]);
     in pkgs.linkFarmFromDrvs "cachix-${host}" (
-      # self.inputs and inputs of inputs, recursively linked
-      (inputFarm inputs) ++
-
       # for modules/system/desktops/cosmic
       #
       # We want to source these from the exact nixpkgs instance that the hosts use. This is
