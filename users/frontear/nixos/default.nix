eaee28f135faba823e478947dd0c89cc2b55090d
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-09-10 14:19:05 -0400

    users: add input group if sway Seems sway needs it for.. something.

--- users/frontear/nixos/default.nix
+++ users/frontear/nixos/default.nix
@@ -12,7 +12,8 @@ lib.mkMerge [
       initialHashedPassword = "$y$j9T$gsXwh6NJa62APePZ.7xR00$lLYi86UgQdN1yjOIgqcegfTKsnqkXI4ufQHWdOTiKr6";
 
       extraGroups = [ "wheel" ] ++
-      (lib.optional config.networking.networkmanager.enable "networkmanager");
+      (lib.optional config.networking.networkmanager.enable "networkmanager") ++
+      (lib.optional config.my.desktops.sway.enable "input");
     };
   })
   # Set the default user in WSL context
