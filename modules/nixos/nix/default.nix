4ce1a0a200669888fbb5b4f4c8306f48ea3c9446
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-09-11 20:59:37 -0400

    modules: /tmp is tmpfs, nix TMPDIR is /var/tmp Doing this I save a lot of boot time that was wasted in cleaning tmp out

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -6,34 +6,30 @@
   ...
 }:
 let
-  inherit (lib) mapAttrs' mapAttrsToList mkDefault mkEnableOption mkForce mkIf mkMerge mkOption types;
-
   # thanks lychee :3
   # https://github.com/itslychee/config/blob/69290575cc0829d40b516654e19d6b789edf32d0/modules/nix/settings.nix
-  inputFarm = pkgs.linkFarm "input-farm" (mapAttrsToList (name: path: {
+  inputFarm = pkgs.linkFarm "input-farm" (lib.mapAttrsToList (name: path: {
     inherit name path;
   }) inputs);
 in {
-  options.my.nix = {
-    enable = (mkEnableOption "nix" // { default = true; });
-    package = mkOption {
-      # https://gist.github.com/Frontear/f88e27b0a5c2841c849a1a21e6b70793
-      default = pkgs.lix;
-
-      type = types.package;
-      internal = true;
-      readOnly = true;
-    };
-  };
-
-  config = mkIf config.my.nix.enable (mkMerge [
+  config = lib.mkIf config.nix.enable (lib.mkMerge [
     {
-      # Enable nix (duh!) and disable channels
-      nix.enable = mkDefault true;
-      nix.channel.enable = mkForce false;
+      # Ensure /var/tmp is a persisted directory (all of var should be persisted anyways)
+      # TODO: move this to saner place
+      my.persist.directories = [ "/var/tmp" ];
+
+      # Encourage Nix to use the (usually larger) /var/tmp instead of /tmp
+      systemd.services.nix-daemon = {
+        environment.TMPDIR = "/var/tmp";
+      };
+    }
+    {
+      # Disable the legacy channels
+      nix.channel.enable = lib.mkForce false;
 
       # Use Lix!
-      nix.package = config.my.nix.package;
+      # https://gist.github.com/Frontear/f88e27b0a5c2841c849a1a21e6b70793
+      nix.package = pkgs.lix;
 
       # Use viper's nix wrapper
       programs.nh.enable = true;
@@ -42,14 +38,13 @@ in {
       # Explicitly set NIX_PATH in both locations, because they don't
       # correctly propagate if channels are disabled.
       # https://github.com/NixOS/nixpkgs/pull/273170
-      nix.nixPath = [ "${inputFarm}" ];
-      nix.settings.nix-path = mkForce config.nix.nixPath;
-    }
-    {
+      nix.nixPath = lib.mkForce [ "${inputFarm}" ];
+      nix.settings.nix-path = lib.mkForce config.nix.nixPath;
+
       # Disable the default nix registry and propagate it
       # with our inputs.
-      nix.settings.flake-registry = mkForce "";
-      nix.registry = mapAttrs' (name: val: {
+      nix.settings.flake-registry = lib.mkForce "";
+      nix.registry = lib.mapAttrs' (name: val: {
         inherit name;
         value.flake = val;
       }) inputs;
@@ -58,7 +53,6 @@ in {
       # Configure nixpkgs with opinionated settings.
       # https://nixos.org/manual/nixpkgs/unstable/#chap-packageconfig
       nixpkgs.config = {
-        allowAliases = true; # TODO: pkgs.system no worky
         allowUnfree = true;
         checkMeta = true;
         warnUndeclaredOptions = true;
@@ -66,7 +60,7 @@ in {
     }
     {
       # Configure nix with opinionated settings.
-      nix.settings = mkMerge (import ./settings.nix);
+      nix.settings = lib.mkMerge (import ./settings.nix);
     }
   ]);
 }
