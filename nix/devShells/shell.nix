8fd3cfc1ee3d0729b80abb0de4b88840bad491c7
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-09-18 15:23:01 -0400

    nix: simplify devshell bringing in scripts

--- nix/devShells/shell.nix
+++ nix/devShells/shell.nix
@@ -1,48 +1,26 @@
 {
   lib,
 
-  stdenv,
-  mkShellNoCC,
+  runCommandLocal,
   makeWrapper,
 
-  gitMinimal,
-  nh,
-  cachix,
-  gnused,
-  jq,
+  gitMinimal, nh, cachix, gnused, jq,
 
-  nil,
+  mkShellNoCC,
+  nil
 }:
-let
-  scripts = stdenv.mkDerivation {
-    pname = "scripts";
-    version = "0.1.2";
-
-    src = ./bin;
-
-    nativeBuildInputs = [
-      makeWrapper
-    ];
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out
-
-      install -Dm755 ./rebuild $out/bin/rebuild
-      install -Dm755 ./gc $out/bin/gc
-      install -Dm755 ./cachix-push $out/bin/cachix-push
-
-      wrapProgram $out/bin/rebuild --prefix PATH : ${lib.makeBinPath [ gitMinimal nh ]}
-      # wrapProgram $out/bin/gc --prefix PATH : ${lib.makeBinPath [ ]}
-      wrapProgram $out/bin/cachix-push --prefix PATH : ${lib.makeBinPath [ cachix gnused jq ]}
-
-      runHook postInstall
-    '';
-  };
-in mkShellNoCC {
+mkShellNoCC {
   packages = [
     nil
-    scripts
+    (runCommandLocal "install-bin" {
+      nativeBuildInputs = [ makeWrapper ];
+    } ''
+      install -Dm755 -t $out/bin ${./bin}/*
+
+      patchShebangs $out
+
+      wrapProgram $out/bin/cachix-push --prefix "PATH" ':' "${lib.makeBinPath [ cachix gnused jq ]}"
+      wrapProgram $out/bin/rebuild --prefix "PATH" ':' "${lib.makeBinPath [ gitMinimal nh ]}"
+    '')
   ];
 }
