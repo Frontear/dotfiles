f544d0eb9f80514ae7b55e6130018359b6994137
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-09-27 12:53:38 -0400

    users/frontear: --no-repeat audio mute key in sway

--- users/frontear/home/sway/config.nix
+++ users/frontear/home/sway/config.nix
@@ -52,7 +52,7 @@ input type:touchpad {
 }
 
 bindsym --locked {
-  XF86AudioMute exec wpctl set-mute @DEFAULT_SINK@ toggle
+  --no-repeat XF86AudioMute exec wpctl set-mute @DEFAULT_SINK@ toggle
   XF86AudioLowerVolume exec wpctl set-volume @DEFAULT_SINK@ 5%-
   XF86AudioRaiseVolume exec wpctl set-volume @DEFAULT_SINK@ 5%+ --limit 1.0
 
