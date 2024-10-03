485f2f7812a64c4b72b3765b9903a293614c6b6a
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-10-02 21:56:32 -0400

    nixos/modules: improve nix module + fix wrapper outputs Closes #13

--- modules/nixos/programs/nix/default.nix
+++ modules/nixos/programs/nix/default.nix
@@ -14,29 +14,57 @@ let
 in {
   config = lib.mkIf config.nix.enable (lib.mkMerge [
     {
-      # Use viper's nix cli wrapper, use my wrapper and provide it Lix as
-      # the Nix implementation
+      # Use github:viperML/nh as our "nix wrapper" program.
       programs.nh.enable = true;
 
+      # Wrap the official nix binary with a snippet to allow
+      # rapid repl access to `pkgs.*` and `lib.*` attributes.
       nix.package = pkgs.callPackage ./package.nix { nix = pkgs.lix; };
     }
     {
       # Throttle the nix-daemon so it doesn't consume
-      # all available memory immediately.
+      # all of our systems' available memory. This
+      # functionality leverages cgroupsv2.
+      #
+      # The logic here is that relying on swap more
+      # will reduce the likelihood of an OOM condition
+      # and overall reduce extreme freezing on our system.
+      nix.settings = {
+        experimental-features = lib.singleton "cgroups";
+        use-cgroups = true;
+      };
+
       systemd.services.nix-daemon.serviceConfig = {
         MemoryHigh = "70%";
-        MemorySwapMax = "95%";
+        MemorySwapMax = "90%";
+      };
+
+      # TODO: determine the usefulness of these
+      # from: https://github.com/nix-community/servos/blob/c98d0acb7c447a85f9f3d751321e9012ea21e8e1/nixos/common/nix.nix
+      nix.daemonCPUSchedPolicy = "batch";
+      nix.daemonIOSchedClass = "idle";
+      nix.daemonIOSchedPriority = 7;
+    }
+    {
+      # Configure nixpkgs with some sane defaults that will
+      # propagate throughout the configuration.
+      # see: https://nixos.org/manual/nixpkgs/unstable/#chap-packageconfig
+      nixpkgs.config = {
+        allowUnfree = true;
+        checkMeta = true;
+        warnUndeclaredOptions = true;
       };
     }
     {
-      # Disable the legacy channels, set nix path to fix breakages from
-      # doing so, and get rid of the stinky flake registry to populate it
-      # with out own stuff.
+      # Disable the legacy channels and set nix path to fix
+      # breakages from doing so.
       nix.channel.enable = lib.mkForce false;
-
+      
       nix.nixPath = lib.mkForce [ "${inputFarm}" ];
       nix.settings.nix-path = lib.mkForce config.nix.nixPath;
-
+    }
+    {
+      # Fully replace the flake registry with relevant inputs.
       nix.settings.flake-registry = lib.mkForce "";
       nix.registry = lib.mapAttrs' (name: val: {
         inherit name;
@@ -44,14 +72,85 @@ in {
       }) inputs;
     }
     {
-      # Configure nix and nixpkgs with opinionated settings.
-      # see (nixpkgs): https://nixos.org/manual/nixpkgs/unstable/#chap-packageconfig
-      nix.settings = lib.mkMerge (import ./settings.nix);
-      nixpkgs.config = {
-        allowUnfree = true;
-        checkMeta = true;
-        warnUndeclaredOptions = true;
-      };
+      # Configure the nix daemon with some opinionated defaults.
+      # see: https://nix.dev/manual/nix/development/command-ref/conf-file.html
+      nix.settings = lib.mkMerge [
+        {
+          # Leverage my custom substituters, courtesy of Cachix.
+          substituters = lib.singleton "https://frontear.cachix.org";
+          trusted-public-keys = lib.singleton "frontear.cachix.org-1:rrVt1C9dFaJf9QpG1Vu6sHqEUy0Q8ezLCCaxz7oZPOM=";
+        }
+        {
+          allow-import-from-derivation = false;
+          auto-optimise-store = true;
+
+          builders-use-substitutes = true;
+
+          connect-timeout = 15;
+          cores = 0;
+
+          debugger-on-trace = true;
+          # debugger-on-warn = true;
+          download-attempts = 3;
+          eval-cache = true;
+
+          # It's useful to know when a substitute is failing!
+          fallback = false;
+
+          # Improve the chances of the store surviving a random crash.
+          fsync-metadata = true;
+          # fsync-store-paths = true;
+
+          # Keeping these is very useful for development.
+          keep-build-log = true;
+          keep-derivations = true;
+          keep-failed = true;
+          keep-outputs = true;
+
+          max-jobs = "auto";
+          min-free = 10 * 1024 * 1024 * 1024;
+
+          preallocate-contents = true;
+
+          # Never allow a non-sandboxed build
+          sandbox-fallback = false;
+
+          show-trace = true;
+          sync-before-registering = true;
+
+          trace-verbose = true;
+          trusted-users = [
+            "root"
+            "@wheel"
+          ];
+
+          use-xdg-base-directories = true;
+
+          # This is such a silly warning.
+          warn-dirty = false;
+        }
+        {
+          # Disallow flake configs by default, and enable automatic
+          # UID allocation as required by the nix builder.
+          accept-flake-config = false;
+          auto-allocate-uids = true;
+
+          # Enable relevant experimental features that are used
+          # by this configuration.
+          experimental-features = [
+            # Relevant for building.
+            "auto-allocate-uids"
+
+            # Critical flags for flakes.
+            "flakes"
+            "nix-command"
+            
+            # Some interesting features.
+            # "fetch-closures"
+            "no-url-literals"
+          ];
+        }
+      ];
     }
   ]);
 }
--- modules/nixos/programs/nix/package.nix
+++ modules/nixos/programs/nix/package.nix
@@ -1,36 +1,42 @@
 {
   lib,
-  symlinkJoin,
+  runCommandLocal,
   writeShellScriptBin,
 
   nix,
 }:
-symlinkJoin {
-  # For the nixos module to lookup the right version
-  name = lib.getName nix;
+let
+  nix-wrapped = writeShellScriptBin "nix" ''
+    declare -a args
+    
+    if [ "$1" = "repl" ]; then
+      # https://wiki.nixos.org/wiki/Flakes#Getting_Instant_System_Flakes_Repl
+      args+=(repl --expr "builtins // { inherit (import <nixpkgs> {}) pkgs lib; }")
+      shift 1
+    fi
+
+    # https://discourse.nixos.org/t/how-do-nix-legacy-commands-work-when-they-are-just-symbolic-links-to-nix/52797
+    cmd=(
+      "$(basename $0)"
+      "''${args[@]}"
+      "$@"
+    )
+
+    PATH="${nix}/bin:$PATH" exec "''${cmd[@]}"
+  '';
+in runCommandLocal "wrap-nix" {
+  pname = lib.getName nix;
   version = lib.getVersion nix;
 
-  paths = [
-    (writeShellScriptBin "nix" ''
-      declare -a args
-      
-      if [ "$1" = "repl" ]; then
-        # https://wiki.nixos.org/wiki/Flakes#Getting_Instant_System_Flakes_Repl
-        args+=(repl --expr "builtins // { inherit (import <nixpkgs> {}) pkgs lib; }")
-        shift 1
-      fi
-
-      # https://discourse.nixos.org/t/how-do-nix-legacy-commands-work-when-they-are-just-symbolic-links-to-nix/52797
-      cmd=(
-        "$(basename $0)"
-        "''${args[@]}"
-        "$@"
-      )
-
-      PATH="${nix}/bin:$PATH" exec "''${cmd[@]}"
-    '')
-    nix
-  ];
+  outputs = nix.outputs;
 
   meta.mainProgram = "nix";
-}
+} ''
+  install -Dm755 -t $out/bin ${lib.getExe nix-wrapped}
+
+  ${lib.concatStringsSep "\n" (map (output: ''
+    mkdir -p ${placeholder output}
+    cp --update=none -rt ${placeholder output} ${nix.${output}}/*
+  ''
+  ) nix.outputs)}
+''
