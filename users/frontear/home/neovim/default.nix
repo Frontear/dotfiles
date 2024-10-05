33a66b87333407c237e02d3851142b876aa513de
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-10-05 01:41:34 -0400

    users/neovim: add columns and softtabstop

--- users/frontear/home/neovim/default.nix
+++ users/frontear/home/neovim/default.nix
@@ -11,9 +11,12 @@
     ];
 
     init = ''
+      vim.opt.cursorline = true
+
       vim.opt.wrap = true
 
       vim.opt.tabstop = 2
+      vim.opt.softtabstop = 2
       vim.opt.shiftwidth = 2
       vim.opt.expandtab = true
 
