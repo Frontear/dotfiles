267c78f55601ed44f555255f0ad7da62799fa66e
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-11-04 22:23:32 -0500

    modules/home-manager: add fd as find replacement

---
+++ modules/home-manager/programs/fd/default.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.my.programs.fd;
+in {
+  options = {
+    my.programs.fd = {
+      enable = lib.mkDefaultEnableOption "fd";
+      package = lib.mkPackageOption pkgs "fd" {};
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [ cfg.package ];
+
+    home.shellAliases = {
+      find = lib.getExe cfg.package;
+    };
+  };
+}
