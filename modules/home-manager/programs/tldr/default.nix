3fd0b845407c529bd79c5928ab8425536755c98f
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-11-06 13:08:37 -0500

    modules/home-manager: update tldr to use newer client This came with the benefit of using an XDG compliant directory, compared to the standard one which is the C client, that is also abandoned.

--- modules/home-manager/programs/tldr/default.nix
+++ modules/home-manager/programs/tldr/default.nix
@@ -6,15 +6,32 @@
 }:
 let
   cfg = config.my.programs.tldr;
+  fmt = pkgs.formats.toml {};
 in {
   options = {
     my.programs.tldr = {
       enable = lib.mkDefaultEnableOption "tldr";
-      package = lib.mkPackageOption pkgs "tldr" {};
+      package = lib.mkPackageOption pkgs "tlrc" {};
+
+      settings = lib.mkOption {
+        default = {};
+
+        type = fmt.type;
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
+    my.programs.tldr.settings = {
+      cache.dir = "${config.xdg.cacheHome}/tlrc";
+    };
+
+    my.persist.directories = [
+      "~/.cache/tlrc"
+    ];
+
     home.packages = [ cfg.package ];
+
+    xdg.configFile."tlrc/config.toml".source = fmt.generate "tlrc-config.toml" cfg.settings;
   };
 }
