48a153385448c7f5dcd4ed52d79572243a75c0d2
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-12-02 20:16:44 -0500

    hosts/laptop: use the xanmod kernel
    Originally I used to use the latest standard kernel, however after 6.11 it would consistently fail large Nix evals. I could not determine the root cause but I did notice that my zram powered swap wouldn't fill up entirely, and yet oom-killer would fire.
    
    So why the switch? I'm honestly interested in the performance patches, and I made sure to check that my system can still evaluate. Thankfully, it does! Regenerating a `nix-index` database used to fail with the latest kernel, but with the latest xanmod it does not. This is a good sign and as a result im merging in.

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   ...
 }:
 {
@@ -15,6 +16,9 @@
       dns.providers.cloudflare.enable = true;
       hosts.providers.stevenblack.enable = true;
     };
+
+    # Use the latest xanmod kernel, mainly for the Clear Linux patches
+    boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
     # Enable a desktop environment
     my.desktops.sway.enable = true;
