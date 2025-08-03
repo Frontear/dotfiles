53a0751860caf8d2e1f5e78964c2b69cef7c1157
Ali Rizvi <contact@frontear.dev>
2025-08-02 20:48:08 -0400

    feat: update `flake.lock` and remove `libirecovery` overlay
    The changes have finally landed in the unstable branch.

--- modules/nixos/programs/libimobiledevice/config.nix
+++ modules/nixos/programs/libimobiledevice/config.nix
@@ -18,19 +18,5 @@ in {
       enable = true;
       package = pkgs.usbmuxd2;
     };
-
-    # TODO: remove this when nixos-unstable has the PR
-    #
-    # see: https://github.com/NixOS/nixpkgs/pull/419291
-    nixpkgs.overlays = [
-      (final: prev: {
-        libirecovery = prev.libirecovery.overrideAttrs {
-          configureFlags = [
-            "--with-udevrulesdir=${placeholder "out"}/lib/udev/rules.d"
-            ''--with-udevrule=OWNER="root",GROUP="myusergroup",MODE="0660"''
-          ];
-        };
-      })
-    ];
   };
 }
