dedadd4380e887e69e4df8360afb3e2686264853
Ali Rizvi <contact@frontear.dev>
2025-08-08 12:00:15 -0400

    feat: fix screenshot binary not appending file extension
    I've also taken the liberty to update the naming convention. Using `%e` is bad because it adds an extra space character to single digit days, making them appear weird in the naming.

--- users/frontear/home/desktops/sway/fs/bin/screenshot/src/screenshot.sh
+++ users/frontear/home/desktops/sway/fs/bin/screenshot/src/screenshot.sh
@@ -8,10 +8,10 @@ export PATH="@path@:$PATH"
 
 ss_dir="$HOME/Pictures/Screenshots"
 ss_folder=$(date '+%Y-%m')
-ss_name=$(date '+%e_%Hh%Mm%Ss')
+ss_name=$(date '+%Y-%m-%d_%Hh%Mm%Ss')
 
 # Ensure folder exists before capturing the screen
 mkdir -p "$ss_dir/$ss_folder"
 
 # Capture the screenshot, save it to the designated path, and copy it to the clipboard.
-grim -cg "$(slurp)" - | tee "$ss_dir/$ss_folder/$ss_name" | wl-copy
+grim -cg "$(slurp)" - | tee "$ss_dir/$ss_folder/$ss_name.png" | wl-copy
