4f369c67e98ea17d44991a3572e25689a6659b0e
Ali Rizvi <contact@frontear.dev>
2025-08-15 16:01:35 -0400

    feat: re-organize stylix definitions for Sway into their own folder

--- users/frontear/home/desktops/sway/default.nix
+++ users/frontear/home/desktops/sway/default.nix
@@ -11,72 +11,12 @@ let
   };
 in {
   imports = [
+    ./stylix
     ./programs
     ./services
   ];
 
   config = lib.mkIf cfg.enable {
-    stylix = {
-      enable = true;
-
-      base16Scheme = ./base16.yaml;
-
-      cursor = {
-        name = "Bibata-Modern-Classic";
-        package = pkgs.bibata-cursors;
-
-        size = 16;
-      };
-
-      fonts = {
-        sizes = {
-          applications = 11;
-          terminal = 8;
-        };
-
-        emoji = {
-          name = "Noto Color Emoji";
-          package = pkgs.noto-fonts-emoji;
-        };
-
-        monospace = {
-          name = "Noto Sans Mono";
-          package = pkgs.noto-fonts;
-        };
-
-        sansSerif = {
-          name = "Noto Sans";
-          package = pkgs.noto-fonts;
-        };
-
-        serif = {
-          name = "Noto Serif";
-          package = pkgs.noto-fonts;
-        };
-      };
-
-      icons = {
-        enable = true;
-        package = pkgs.papirus-icon-theme;
-
-        dark = "Papirus-Dark";
-        light = "Papirus-Light";
-      };
-
-      image = ./fs/sway/backgrounds/bg_dark.jpg;
-      imageScalingMode = "fit";
-
-      polarity = "dark";
-    };
-
-    stylix.targets = {
-      fontconfig.enable = true;
-      font-packages.enable = true;
-
-      gtk.enable = true;
-      qt.enable = true;
-    };
-
     my.desktops.sway.config = "${fs.sway}/config";
 
     my.programs = {
