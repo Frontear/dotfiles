4ca1dfb04797ee5a17a695bd7c96e8ec753460c3
Ali Rizvi <contact@frontear.dev>
2025-09-21 00:49:14 -0400

    feat: improve Niri desktop environment
    Firstly, we've finally added a proper polkit agent. Sway never had one, Niri is the first. I chose to use GNOME's agent since Niri already uses a lot of GNOME/GTK things as defaults and/or recommendations.
    
    Second, I overrode the Niri XDG Desktop Portal configuration to accept any file manager for the "file chooser" dialog. The GNOME Portal, which was preferred, enforced Nautilus, and I don't use Nautilus. Rather, I use Thunar, so I needed to overwrite this.
    
    Lastly, I added a new keybind Mod+L to lock the screen, and I updated the Mod+{Down,Up} and Mod+Shift+{Down,Up} keybinds to move workspaces when possible. I wanted to try and bend Niri to make my workspaces feel static, but I'll be flexible and try to make this dynamic workspace stuff work.

--- modules/nixos/home-manager/my/desktops/niri/config.nix
+++ modules/nixos/home-manager/my/desktops/niri/config.nix
@@ -7,6 +7,10 @@ let
   cfg = config.my.desktops.niri;
 in {
   config = lib.mkIf cfg.enable {
+    # Since Niri already uses a lot of GNOME things, let's pull the GNOME
+    # polkit authentication agent as well.
+    services.polkit-gnome.enable = true;
+
     xdg.configFile."niri/config.kdl".source = cfg.config;
   };
 }
--- modules/nixos/my/desktops/niri/config.nix
+++ modules/nixos/my/desktops/niri/config.nix
@@ -33,5 +33,19 @@ in {
     programs.niri = {
       enable = true;
     };
+
+    # This is being overriden from what Niri provides by default so that it can
+    # support other file-managers besides Nautilus, since the GNOME Portal only
+    # defaults to Nautilus for the file chooser dialog.
+    #
+    # see: https://github.com/YaLTeR/niri/blob/a1dccedbb72da372d2a8a84022f37ccaa4d4a6e6/resources/niri-portals.conf
+    xdg.portal.config.niri = {
+      default = [ "gnome" "gtk" ];
+
+      "org.freedesktop.impl.portal.Access" = [ "gtk" ];
+      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+      "org.freedesktop.impl.portal.Notification" = [ "gtk" ];
+      "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+    };
   };
 }
