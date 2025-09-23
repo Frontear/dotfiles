a44c0a2fdfb9361f860a9d7883917a45dc706be4
Ali Rizvi <contact@frontear.dev>
2025-09-23 19:21:56 -0400

    feat: fix `yubioath-flutter` failing to load
    You may notice that my GPG public key has slightly changed. Long story short, my subkeys expired and during the time that I took to renew them, I decided to delete my old identity off of the key. I made this change gracefully, by keeping both identities for almost a year before now erasing the old one. Going forward, this is my one and only official identity for online purposes.

--- modules/nixos/home-manager/my/services/yubico/config.nix
+++ modules/nixos/home-manager/my/services/yubico/config.nix
@@ -7,6 +7,15 @@
 }:
 let
   cfg = nixosConfig.my.services.yubico;
+
+  # Temporary extension to `pkgs` to fix an upstream incompatibility between
+  # `yubikey-manager` and `yubioath-flutter`.
+  #
+  # TODO: drop with https://github.com/NixOS/nixpkgs/issues/442315
+  pkgs' = pkgs.extend (final: prev: {
+    # from: https://github.com/NixOS/nixpkgs/blob/3a75333d2d9e9114e0d8223f8bcd9d3b32c6401f/pkgs/by-name/yu/yubikey-manager/package.nix
+    yubikey-manager = prev.callPackage ./yubikey-manager.nix {};
+  });
 in {
   config = lib.mkIf cfg.enable {
     my.persist.directories = [
@@ -20,7 +29,7 @@ in {
       }
     ];
 
-    home.packages = with pkgs; [
+    home.packages = with pkgs'; [
       yubikey-manager
       yubioath-flutter
     ];
