7e1bb72882f7b1d18adc4d573a6f106f0bacf222
Ali Rizvi <contact@frontear.dev>
2025-09-26 18:23:24 -0400

    feat: default `zramSwap.enable = true`, use GNOME for ISO
    Using GNOME for the ISO instead of KDE will reduce the overall build time on my system because it can re-use many of the derivations from my GNOME specialisation, rather than needing to pull the KDE ones. Build times shot down significantly by doing this change, so I'm pretty happy with it overall.
    
    I've also enabled **zram as swap** configuration by default for my system, simply because I don't use swap files/swap devices, and don't plan to.

--- hosts/iso/configuration.nix
+++ hosts/iso/configuration.nix
@@ -8,21 +8,13 @@
   ];
 
   config = {
-    networking.networkmanager.enable = true;
+    my.desktops.gnome.enable = true;
 
-    services.desktopManager.plasma6.enable = true;
-    services.displayManager.defaultSession = "plasmax11";
-
-    services.displayManager = {
-      autoLogin.enable = true;
-      sddm.enable = true;
-    };
+    services.displayManager.autoLogin.enable = true;
 
     console.keyMap = "us";
     i18n.defaultLocale = "en_CA.UTF-8";
     time.timeZone = "America/Toronto";
-
-    zramSwap.enable = true;
 
     nixpkgs.config.allowUnfree = true;
   };
--- hosts/laptop/hardware/mounts.nix
+++ hosts/laptop/hardware/mounts.nix
@@ -37,7 +37,5 @@
 
       interval = "weekly";
     };
-
-    zramSwap.enable = true;
   };
 }
