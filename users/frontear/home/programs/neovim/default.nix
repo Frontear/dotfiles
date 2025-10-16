19634f2dd0a0f48e13569ab7b0856c0493fed672
Ali Rizvi <contact@frontear.dev>
2025-10-16 16:28:54 -0400

    fix: migrate `require("lspconfig")` to `vim.lsp.config`
    This was a change made upstream in https://www.github.com/neovim/nvim-lspconfig/issues/3693. In a nutshell, `require("lspconfig").<lsp>.setup` is no longer the accepted way to configure language servers. Instead, one should use `vim.lsp.config.<lsp> = {}`.
    
    The migration itself is fairly trivial, though we also need to throw `vim.lsp.enable("<lsp>")` to activate it.

--- users/frontear/home/programs/neovim/default.nix
+++ users/frontear/home/programs/neovim/default.nix
@@ -102,15 +102,16 @@
 
           --lsp_format.setup {}
 
-          local lspconfig = require("lspconfig")
           local capabilities = require("cmp_nvim_lsp").default_capabilities()
           --local on_attach = lsp_format.on_attach
 
           ${lib.concatStringsSep "\n" (map (lsp: ''
-            lspconfig.${lsp}.setup({
+            vim.lsp.config["${lsp}"] = {
               capabilities = capabilities,
               --on_attach = on_attach
-            })
+            }
+
+            vim.lsp.enable("${lsp}")
           '') [
             "basedpyright"
             "clangd"
