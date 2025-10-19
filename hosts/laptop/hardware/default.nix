c72d1f7803847e614679a433688c522bffbbf693
Ali Rizvi <contact@frontear.dev>
2025-10-19 00:32:00 -0400

    fix: strictly specify nixos-hardware modules
    This lets me explicitly pick the tiger-lake modules, which brings some better defaults and actually reduces closure size, since it doesn't need to pull extra things that are useless for my system.

--- hosts/laptop/hardware/default.nix
+++ hosts/laptop/hardware/default.nix
@@ -3,4 +3,11 @@
     ./mounts.nix
     ./power-savings
   ];
+
+  config = {
+    services = {
+      fwupd.enable = true;
+      thermald.enable = true;
+    };
+  };
 }
--- nix/nixosConfigurations/default.nix
+++ nix/nixosConfigurations/default.nix
@@ -15,8 +15,11 @@ in {
       {
         hostName = "LAPTOP-3DT4F02";
         modules = [
-          nixos-hardware.nixosModules.dell-inspiron-14-5420
-          nixos-hardware.nixosModules.common-hidpi
+          "${nixos-hardware}/common/cpu/intel/tiger-lake/cpu-only.nix"
+          "${nixos-hardware}/common/gpu/intel/tiger-lake"
+          "${nixos-hardware}/common/pc/laptop"
+          "${nixos-hardware}/common/pc/ssd"
+          "${nixos-hardware}/common/hidpi.nix"
 
           ../../hosts/laptop
         ];
