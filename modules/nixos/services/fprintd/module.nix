955cc22c9648c1fec7a6f4a5d1f9505382abf552
Ali Rizvi <contact@frontear.dev>
2025-10-19 15:41:30 -0400

    feat: disallow fingerprint authentication for login
    Since fingerprints cannot be used to decrypt, GNOME keyring is unable to be decrypted at login and will instead pester the user for the password post-login. This is not an easy problem to solve from both practical and philosphical angles, so the best solution is to simply disallow it.

--- modules/nixos/services/fprintd/module.nix
+++ modules/nixos/services/fprintd/module.nix
@@ -5,11 +5,43 @@
 }:
 let
   cfg = config.services.fprintd;
+
+  cfg-gdm = config.services.displayManager.gdm;
+  cfg-greetd = config.services.greetd;
 in {
-  config = lib.mkIf cfg.enable {
-    my.persist.directories = [{
-      path = "/var/lib/fprint";
-      unique = false;
-    }];
-  };
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      my.persist.directories = [{
+        path = "/var/lib/fprint";
+        unique = false;
+      }];
+    }
+
+    # Prevent fingerprint login, as it breaks UX expectations and has no
+    # straightforward solution.
+    #
+    # NOTE: this isn't unique to GDM, despite the issue being filed there.
+    # see: https://gitlab.gnome.org/GNOME/gdm/-/issues/613
+    {
+      programs.dconf.profiles = lib.mkIf cfg-gdm.enable {
+        gdm.databases = [{
+          lockAll = true;
+
+          settings = {
+            "org/gnome/login-screen" = {
+              enable-fingerprint-authentication = false;
+            };
+          };
+        }];
+      };
+
+      security.pam.services = lib.mkMerge [
+        { "login".fprintAuth = false; }
+
+        (lib.mkIf cfg-greetd.enable {
+          "greetd".fprintAuth = false;
+        })
+      ];
+    }
+  ]);
 }
