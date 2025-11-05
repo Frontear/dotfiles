16704b609a9d3ebc96be13cb5f72a3a74bbee813
Ali Rizvi <contact@frontear.dev>
2025-11-05 18:05:03 -0500

    fix: check cpu/gpu type for facter module

--- modules/nixos/facter/report/hardware/graphics_card/module.nix
+++ modules/nixos/facter/report/hardware/graphics_card/module.nix
@@ -4,14 +4,19 @@
   pkgs,
   ...
 }:
-# TODO: assumes tigerlake CPU/GPU, assumes i915 kernel module
 let
   usingFacter = config.facter.reportPath != null;
 
+  # TODO: migrate to `xe` module once stable
   hasIntelGpu =
     lib.any (x: x.driver == "i915") config.facter.report.hardware.graphics_card;
+
+  isTigerlake = lib.any (x:
+    x.family == 6 &&
+    x.model == 140
+  ) config.facter.report.hardware.cpu;
 in {
-  config = lib.mkIf (usingFacter && hasIntelGpu) {
+  config = lib.mkIf (usingFacter && hasIntelGpu && isTigerlake) {
     boot.kernelParams = [ "i915.enable_guc=3" ];
 
     hardware.graphics.extraPackages = with pkgs; [
