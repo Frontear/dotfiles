b3c045717945d43ade35d78b667223932577688e
Ali Rizvi <contact@frontear.dev>
2025-11-15 00:45:38 -0500

    fix: move specialisations to their own file

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,29 +1,16 @@
 {
   pkgs,
-  lib,
   ...
 }:
 {
   imports = [
     ./hardware
+    ./specialisations
   ];
 
   config = {
-    specialisation.gnome.configuration = {
-      services.auto-cpufreq.enable = lib.mkForce false;
-
-
-      my.desktops = {
-        niri.enable = lib.mkForce false;
-        gnome.enable = true;
-      };
-    };
-
     # Use the latest xanmod kernel, mainly for the Clear Linux patches
     boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
-
-    # Enable a desktop environment
-    my.desktops.niri.enable = true;
 
     # Set locale, keymap and timezone
     console.keyMap = "us";
