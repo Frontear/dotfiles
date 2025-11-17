24283379e7830ced7d12c023d94f47c9b8716125
Ali Rizvi <contact@frontear.dev>
2025-11-17 18:01:43 -0500

    fix: refactor chrony into module, cleanup hosts/laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -12,6 +12,16 @@
     # Use the latest xanmod kernel, mainly for the Clear Linux patches
     boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
+    services = {
+      # NTP daemon that's more suitable for laptops
+      chrony.enable = true;
+
+      # Use the fingerprint sensor on my laptop.
+      #
+      # TODO: detect from `facter.json`
+      fprintd.enable = true;
+    };
+
     # Set locale, keymap and timezone
     console.keyMap = "us";
     i18n.defaultLocale = "en_CA.UTF-8";
--- hosts/laptop/hardware/default.nix
+++ hosts/laptop/hardware/default.nix
@@ -1,12 +1,5 @@
 {
   imports = [
     ./mounts.nix
-    ./power-savings
   ];
-
-  config = {
-    services = {
-      fprintd.enable = true;
-    };
-  };
 }
