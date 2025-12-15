6f47bd8973e813876240ef51ea67e847e8edde8b
Ali Rizvi <contact@frontear.dev>
2025-12-15 12:55:15 -0500

    fix: persist OpenRazer and Polychromatic paths
    The most important of these is the Polychromatic cache, and the OpenRazer config. I'm unsure if I really care much about the Polychromatic config, but for now I'm keeping it.

--- modules/shared/hardware/openrazer/config/home-manager.nix
+++ modules/shared/hardware/openrazer/config/home-manager.nix
@@ -11,5 +11,28 @@ in {
     home.packages = with pkgs; [
       polychromatic
     ];
+
+    # NOTE: I am not saving `~/.local/share/openrazer` because this only
+    # contains logs. Logs are not necessary to see unless there's a problem,
+    # and I can look at them during the problematic run.
+    my.persist.directories = [
+      {
+        # Stores the downloaded images of Razer devices for the UI.
+        path = "~/.cache/polychromatic";
+        unique = false;
+      }
+      {
+        # DPI and polling rate are saved here, in `persistence.conf`.
+        path = "~/.config/openrazer";
+        unique = true;
+      }
+      {
+        # Various UI settings, and the DPI stages (I think?).
+        #
+        # TODO: is this necessary to store?
+        path = "~/.config/polychromatic";
+        unique = true;
+      }
+    ];
   };
 }
