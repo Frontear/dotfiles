1f87cf20d3ec26b30f0e5837a61d3704364d0688
Ali Rizvi <contact@frontear.dev>
2026-03-27 20:38:39 -0400

    feat: drop in-tree overrides for packages

--- modules/nixos/documentation/man/module.nix
+++ modules/nixos/documentation/man/module.nix
@@ -12,12 +12,7 @@ in {
     # They usually exist on most other distros, and I'm very used to that
     # availability, so let's bring them back here too.
     environment.systemPackages = with pkgs; [
-      # Something changed in the `linux-manual` build, and the fix PR hasn't
-      # laned in unstable, so we pull it here.
-      #
-      # TODO: drop this fix once the relevant PR is merged
-      # see: https://github.com/NixOS/nixpkgs/issues/489956
-      (callPackage ./package.nix {})
+      linux-manual
       man-pages
     ];
   };
