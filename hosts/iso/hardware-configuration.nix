76c28b6e226a85100498b118da8432af2c93d0e2
Ali Rizvi <contact@frontear.dev>
2026-03-30 01:23:41 -0400

    feat: enable NVIDIA GPU support in ISO

--- hosts/iso/hardware-configuration.nix
+++ hosts/iso/hardware-configuration.nix
@@ -4,12 +4,20 @@
   ...
 }:
 {
-  config.hardware = {
-    enableAllFirmware = pkgs.config.allowUnfree;
-    enableRedistributableFirmware = lib.mkForce true;
+  config = {
+    hardware = {
+      enableAllFirmware = pkgs.config.allowUnfree;
+      enableRedistributableFirmware = lib.mkForce true;
 
-    cpu = lib.genAttrs [ "amd" "intel" ] (_: {
-      updateMicrocode = true;
-    });
+      cpu = lib.genAttrs [ "amd" "intel" ] (_: {
+        updateMicrocode = true;
+      });
+    };
+
+    # Enable NVIDIA support for installation hosts using their GPUs
+    hardware.graphics.enable = true;
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.open = true; # NOTE: only supports RTX 20+, or GTX 16+
+    hardware.nvidia.modesetting.enable = true; # needed for Wayland
   };
 }
