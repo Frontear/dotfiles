fa2f42b5bfc15a5a22d377a48cd5516ead08d880
Ali Rizvi <contact@frontear.dev>
2026-06-18 18:02:16 -0400

    feat: update `flake.lock`, remove `dnsmasq` fix from virt-manager module

--- modules/nixos/programs/virt-manager/module.nix
+++ modules/nixos/programs/virt-manager/module.nix
@@ -16,18 +16,5 @@ in {
         virtiofsd
       ];
     };
-
-    # Disable stevenblack blocklist, as some of the host entries
-    # cause erroneus faults in `dnsmasq` due to an upstream bug
-    # between `dnsmasq` and `glibc`. Replacing long entries and/or
-    # removing specific entries is insufficient, and recompiling
-    # `dnsmasq` across all dependents is impossible, so this is the
-    # best fix.
-    #
-    # TODO: remove when the relevant PR is merged.
-    #
-    # see: https://github.com/NixOS/nixpkgs/issues/525573
-    # see: https://github.com/NixOS/nixpkgs/pull/528905
-    networking.stevenblack.enable = lib.mkForce false;
   };
 }
